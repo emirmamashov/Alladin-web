3662c9444058ff090dc5756e02b4eed63e942766
Emir Mamashov <emir.mamashov@gmail.com>
2017-08-22 19:57:42 +0600

    prod

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
         ROOT_DIR: __dirname,
         DB_URL: process.env.DB_URL || 'mongodb://localhost/alladin',
-        API_URL: 'http://localhost:3000' // 'http://176.126.167.128:3000'
+        API_URL: 'http://176.126.167.128:3000' // 'http://localhost:3000'
     },
     production: {
         ROOT_DIR: __dirname,
