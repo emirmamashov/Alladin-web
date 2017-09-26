d3706e9b074a2b967071599a81de1a1e0cf7278e
Emir Mamashov <emir.mamashov@gmail.com>
2017-09-26 20:24:20 +0600

    codereview index

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ module.exports = (app, db) => {
     router.get('/', (req, res) => {
         db.Category.find({ parentCategory: null }).limit(20).then(
             (parentCategories) => {
-                let categoriesViewInLikesBlock = parentCategories.filter(x => x.viewInLikeBlock) || [];
                 let chunkCategories = chunkService(parentCategories.filter(x => x.image), 4);
                 let categoriesLeft = parentCategories.filter(x => x.showInMainPageLeft);
                 let categoriesRight = parentCategories.filter(x => x.showInMainPageRight);
@@ -36,74 +35,88 @@ module.exports = (app, db) => {
                                 childCategories.forEach((parentCategory) => {
                                     childCategories['childCategories'] = secondCategories.filter(x => x.parentCategory == parentCategory.id) || [];
                                 });
-        
-                                db.Product.find({ isHot: true }).then(
-                                    (hotProducts) => {
-                                        db.Exchange.findOne().then(
-                                            (exchange) => {
-                                                for (let i = 0; i < hotProducts.length > 0; i++) {
-                                                    hotProducts[i]['apiUrl'] = config.API_URL;
-                                                    if (hotProducts[i].price && hotProducts[i].priceStock &&
-                                                        hotProducts[i].price > 0 && hotProducts[i].priceStock > 0) {
-                                                        let differencePercent = (hotProducts[i].priceStock / hotProducts[i].price) * 100; // процент от числа
-                                                        hotProducts[i]['percent'] = Math.round(100 - differencePercent);
-                                                    }
-                                                }
-                
-                                                db.Banner.find().then(
-                                                    (banners) => {
-                                                        banners.forEach((banner) => {
-                                                            banner['apiUrl'] = config.API_URL;
-                                                        });
-                
-                                                        db.Producer.find().then(
-                                                            (producers) => {
-                                                                producers.forEach((producer) => {
-                                                                    producer['apiUrl'] = config.API_URL;
+
+                                db.Category.find({ viewInLikeBlock: true }).then(
+                                    (categoriesViewInLikesBlock) => {
+                                        db.Product.find({ isHot: true }).then(
+                                            (hotProducts) => {
+                                                db.Exchange.findOne().then(
+                                                    (exchange) => {
+                                                        for (let i = 0; i < hotProducts.length > 0; i++) {
+                                                            hotProducts[i]['apiUrl'] = config.API_URL;
+                                                            if (hotProducts[i].price && hotProducts[i].priceStock &&
+                                                                hotProducts[i].price > 0 && hotProducts[i].priceStock > 0) {
+                                                                let differencePercent = (hotProducts[i].priceStock / hotProducts[i].price) * 100; // процент от числа
+                                                                hotProducts[i]['percent'] = Math.round(100 - differencePercent);
+                                                            }
+                                                        }
+
+                                                        db.Banner.find().then(
+                                                            (banners) => {
+                                                                banners.forEach((banner) => {
+                                                                    banner['apiUrl'] = config.API_URL;
                                                                 });
-                
-                                                                db.Product.find().limit(30).then(
-                                                                    (rndProducts) => {
-                                                                        rndProducts.forEach((product) => {
-                                                                            product['apiUrl'] = config.API_URL;
-                                                                            if (exchange && exchange.usd) {
-                                                                                let price = parseFloat(product.price) || 0;
-                                                                                let priceTrade = parseFloat(product.priceTrade) || 0;
-                                                                                let priceStock = parseFloat(product.priceStock) || 0;
-                                                                                product.price = (price * exchange.usd).toFixed(2);
-                                                                                product.priceTrade = (priceTrade * exchange.usd).toFixed(2);
-                                                                                product.priceStock = (priceStock * exchange.usd).toFixed(2);
+                        
+                                                                db.Producer.find().then(
+                                                                    (producers) => {
+                                                                        producers.forEach((producer) => {
+                                                                            producer['apiUrl'] = config.API_URL;
+                                                                        });
+                        
+                                                                        db.Product.find().limit(30).then(
+                                                                            (rndProducts) => {
+                                                                                rndProducts.forEach((product) => {
+                                                                                    product['apiUrl'] = config.API_URL;
+                                                                                    if (exchange && exchange.usd) {
+                                                                                        let price = parseFloat(product.price) || 0;
+                                                                                        let priceTrade = parseFloat(product.priceTrade) || 0;
+                                                                                        let priceStock = parseFloat(product.priceStock) || 0;
+                                                                                        product.price = (price * exchange.usd).toFixed(2);
+                                                                                        product.priceTrade = (priceTrade * exchange.usd).toFixed(2);
+                                                                                        product.priceStock = (priceStock * exchange.usd).toFixed(2);
+                                                                                    }
+                                                                                });
+                                                                                categoriesViewInLikesBlock.forEach((category) => {
+                                                                                    category['rndProducts'] = rndProducts.filter(x => x.categoryId && x.categoryId.toString() === category._id.toString());
+                                                                                });
+                                                                                let leftBannerShowInMainPage = banners.filter(x => x.showInMainPageLeft)[0];
+                                                                                let rigthBannerShowInMainPage = banners.filter(x => x.showInMainPageRight)[0];
+                        
+                                                                                let chunkProducers = chunkService(producers, 5);
+                                                                                let producersLeft = {};
+                                                                                let producersRight = {};
+                                                                                if (chunkProducers[0] && chunkProducers[0].data) producersLeft = chunkProducers[0].data;
+                                                                                if (chunkProducers[1] && chunkProducers[1].data) producersRight = chunkProducers[1].data;
+                        
+                                                                                res.render('index', {
+                                                                                    title: 'Главная страница',
+                                                                                    categories: parentCategories,
+                                                                                    categoriesViewInMenu: parentCategories.filter(x => x.viewInMenu),
+                                                                                    apiUrl: config.API_URL,
+                                                                                    banners: banners,
+                                                                                    leftBannerShowInMainPage: leftBannerShowInMainPage,
+                                                                                    rigthBannerShowInMainPage: rigthBannerShowInMainPage,
+                                                                                    producers: producers,
+                                                                                    producersLeft: producersLeft,
+                                                                                    producersRight: producersRight,
+                                                                                    categoriesViewInLikesBlock: categoriesViewInLikesBlock,
+                                                                                    categoriesLeft: categoriesLeft,
+                                                                                    categoriesRight: categoriesRight,
+                                                                                    rndProducts: rndProducts,
+                                                                                    hotProducts: hotProducts
+                                                                                });
                                                                             }
-                                                                        });
-                                                                        categoriesViewInLikesBlock.forEach((category) => {
-                                                                            category['rndProducts'] = rndProducts.filter(x => x.categoryId && x.categoryId.toString() === category._id.toString());
-                                                                        });
-                                                                        let leftBannerShowInMainPage = banners.filter(x => x.showInMainPageLeft)[0];
-                                                                        let rigthBannerShowInMainPage = banners.filter(x => x.showInMainPageRight)[0];
-                
-                                                                        let chunkProducers = chunkService(producers, 5);
-                                                                        let producersLeft = {};
-                                                                        let producersRight = {};
-                                                                        if (chunkProducers[0] && chunkProducers[0].data) producersLeft = chunkProducers[0].data;
-                                                                        if (chunkProducers[1] && chunkProducers[1].data) producersRight = chunkProducers[1].data;
-                
-                                                                        res.render('index', {
-                                                                            title: 'Главная страница',
-                                                                            categories: parentCategories,
-                                                                            categoriesViewInMenu: parentCategories.filter(x => x.viewInMenu),
-                                                                            apiUrl: config.API_URL,
-                                                                            banners: banners,
-                                                                            leftBannerShowInMainPage: leftBannerShowInMainPage,
-                                                                            rigthBannerShowInMainPage: rigthBannerShowInMainPage,
-                                                                            producers: producers,
-                                                                            producersLeft: producersLeft,
-                                                                            producersRight: producersRight,
-                                                                            categoriesViewInLikesBlock: categoriesViewInLikesBlock,
-                                                                            categoriesLeft: categoriesLeft,
-                                                                            categoriesRight: categoriesRight,
-                                                                            rndProducts: rndProducts,
-                                                                            hotProducts: hotProducts
-                                                                        });
+                                                                        ).catch(
+                                                                            (err) => {
+                                                                                console.log(err);
+                                                                                res.render('index', {
+                                                                                    title: 'Express123',
+                                                                                    parentCategories: categoryService.findParentCategory(categories),
+                                                                                    errors: err,
+                                                                                    apiUrl: config.API_URL
+                                                                                });
+                                                                            }
+                                                                        );
                                                                     }
                                                                 ).catch(
                                                                     (err) => {
@@ -123,6 +136,7 @@ module.exports = (app, db) => {
                                                                 res.render('index', {
                                                                     title: 'Express123',
                                                                     parentCategories: categoryService.findParentCategory(categories),
+                                                                    products: [],
                                                                     errors: err,
                                                                     apiUrl: config.API_URL
                                                                 });
@@ -135,26 +149,25 @@ module.exports = (app, db) => {
                                                         res.render('index', {
                                                             title: 'Express123',
                                                             parentCategories: categoryService.findParentCategory(categories),
-                                                            products: [],
                                                             errors: err,
                                                             apiUrl: config.API_URL
                                                         });
                                                     }
                                                 );
                                             }
-                                        ).catch(
-                                            (err) => {
-                                                console.log(err);
-                                                res.render('index', {
-                                                    title: 'Express123',
-                                                    parentCategories: categoryService.findParentCategory(categories),
-                                                    errors: err,
-                                                    apiUrl: config.API_URL
-                                                });
-                                            }
-                                        );
+                                        )
                                     }
-                                )
+                                ).catch(
+                                    (err) => {
+                                        console.log(err);
+                                        res.render('index', {
+                                            title: 'Express123',
+                                            parentCategories: categoryService.findParentCategory(categories),
+                                            errors: err,
+                                            apiUrl: config.API_URL
+                                        });
+                                    }
+                                );
                             }
                         ).catch(
                             (err) => {
