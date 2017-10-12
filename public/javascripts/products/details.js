7bc462117e4ecb96aa80155e73acbff3e71bb1ae
Emir Mamashov <emir.mamashov@gmail.com>
2017-10-12 16:39:26 +0600

    При увеличении количество товаров увеливается общая цена соответственно

--- public/javascripts/products/details.js
+++ public/javascripts/products/details.js
@@ -21,18 +21,18 @@ function viewedProductDetails() {
 function setTotalPrice() {
     let count = $('#total-amount').val();
     let price = $('#price').val();
-    $('#totalPrice')[0].textContent = parseFloat(price) * parseFloat(count) + ' сом';
+    $('#totalPrice')[0].textContent = (parseFloat(price) * parseFloat(count)).toFixed(2) + ' сом';
 }
 
 function plusProduct(price) {
     let totalPriceNumber = $('#totalPrice')[0].textContent;
     let totalPrice = parseFloat(totalPriceNumber) + parseFloat(price);
-    $('#totalPrice')[0].textContent = totalPrice + ' сом';
+    $('#totalPrice')[0].textContent = totalPrice.toFixed(2) + ' сом';
 }
 function minusProduct(price) {
     let totalPriceNumber = parseFloat($('#totalPrice')[0].textContent);
     if (totalPriceNumber > price) {
         let totalPrice = totalPriceNumber - parseFloat(price);
-        $('#totalPrice')[0].textContent = totalPrice + ' сом';
+        $('#totalPrice')[0].textContent = totalPrice.toFixed(2) + ' сом';
     }
 }
